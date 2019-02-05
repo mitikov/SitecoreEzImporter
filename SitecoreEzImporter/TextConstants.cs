3a4a517eef6ed99640b2301a0173e4dfffa8ff7d
Nikolay Mitikov <mitikov@users.noreply.github.com>
2019-02-05 14:51:52 +0800

    Introduce TextConstants to escape from hardcodes solution-wide

---
+++ SitecoreEzImporter/TextConstants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EzImporter
+{
+    public static class TextConstants
+    {
+        /// <summary>
+        /// The name of work-in-progress database.
+        /// <para>Defaults to "master".</para>
+        /// </summary>
+        public static readonly string ContentDatabaseName = "master";
+
+
+        public static class PipelineNames
+        {
+            /// <summary>
+            /// The name of import items pipeline.
+            /// <para>Defaults to "importItems"</para>
+            /// </summary>
+            public static readonly string ImportItems = "importItems";
+        }
+    }
+}
