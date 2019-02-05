001479770b1e3880ee2873283a06f434ce1d857b
Nikolay Mitikov <mitikov@users.noreply.github.com>
2019-02-05 16:46:00 +0800

    Add services module configurator

---
+++ SitecoreEzImporter/Services/ServicesConfigurator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace EzImporter.Configuration
+{
+    /// <summary>
+    /// Registers services neeeded for Importer execution.
+    /// <para>Does not override/touch any default service, simply adds own <see cref="ImportOptionsFactory"/>.</para>
+    /// </summary>
+    public class EzImporterServicesConfigurator : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<ImportOptionsFactory, DefaultImportOptionsFactory>();
+        }
+    }
+}
